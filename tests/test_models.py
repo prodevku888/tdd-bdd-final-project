6983fa88bddd6b0a2b32050327843ef7c7a2c740
r4diorusak <r4diorusak@users.noreply.github.com>
2025-11-22 22:33:41 +0700

    Add DELETE test case for Product model

--- tests/test_models.py
+++ tests/test_models.py
@@ -140,3 +140,12 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(products[0].id, original_id)
         self.assertEqual(products[0].description, "testing")
 
+    def test_delete_a_product(self):
+        """It should Delete a Product"""
+        product = ProductFactory()
+        product.create()
+        self.assertEqual(len(Product.all()), 1)
+        # delete the product and make sure it isn't in the database
+        product.delete()
+        self.assertEqual(len(Product.all()), 0)
+
