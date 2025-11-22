3e08be3df3d67a7fe10fa0eb61196fdf0cadcc4a
r4diorusak <r4diorusak@users.noreply.github.com>
2025-11-22 22:34:39 +0700

    Add LIST ALL test case for Product model

--- tests/test_models.py
+++ tests/test_models.py
@@ -149,3 +149,15 @@ class TestProductModel(unittest.TestCase):
         product.delete()
         self.assertEqual(len(Product.all()), 0)
 
+    def test_list_all_products(self):
+        """It should List all Products in the database"""
+        products = Product.all()
+        self.assertEqual(products, [])
+        # Create 5 Products
+        for _ in range(5):
+            product = ProductFactory()
+            product.create()
+        # See if we get back 5 products
+        products = Product.all()
+        self.assertEqual(len(products), 5)
+
